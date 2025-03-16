12122830a4efe73d23c358a9a1cb81f6424163c3
Oleh Sencha <108219585+Pro100grammist@users.noreply.github.com>
2025-03-16 18:46:22 +0200

    Fix environ usage for production on Render

--- artist_portfolio/artist_portfolio/settings.py
+++ artist_portfolio/artist_portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import environ
 
 from pathlib import Path
 
@@ -22,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 ENVIRONMENT = os.getenv("DJANGO_ENV", "development")
+env = environ.Env()
 
 if ENVIRONMENT == "development":
-    import environ
-    env = environ.Env()
     environ.Env.read_env()
 
 SECRET_KEY = env("SECRET_KEY")
