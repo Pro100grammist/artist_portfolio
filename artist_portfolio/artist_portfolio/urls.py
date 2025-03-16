ec11eb1efc8a7d6b2fd3ed63b5d80b6e05eaf9da
Oleh Sencha <108219585+Pro100grammist@users.noreply.github.com>
2025-03-16 21:29:21 +0200

    fix urlpatterns for local DEBUG

--- artist_portfolio/artist_portfolio/urls.py
+++ artist_portfolio/artist_portfolio/urls.py
@@ -45,4 +45,7 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
